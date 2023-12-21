d953798c2aaf6821a8780df1dc435ed80ef4eb0e
Gonçalo Sousa <up202207320@up.pt>
2023-12-21 01:16:10 +0000

    Updated Graph.h, Airport and Loader headers added, Airport.cpp and main.cpp added, not compiling yet

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Sílvia Augusto/Desktop/2proj_AED_G106
+# Install script for directory: C:/Users/Sílvia Augusto/Desktop/2projAED
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Sílvia Augusto/Desktop/2proj_AED_G106/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Sílvia Augusto/Desktop/2projAED/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
