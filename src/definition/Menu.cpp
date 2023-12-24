642313d773289f22b41faf70a7d3dca6cd7919ad
Gonçalo Sousa <up202207320@up.pt>
2023-12-24 19:40:05 +0000

    Updated Menu

--- src/definition/Menu.cpp
+++ src/definition/Menu.cpp
@@ -9,7 +9,8 @@ void Menu::displayMenu(Graph ap) {
         std::cout << "-----------\n" ;
         std::cout << "1. Check the global number of airports\n";
         std::cout << "2. Check the number of available flights\n";
-        std::cout << "\n3. Exit\n";
+        std::cout << "3. Show Airports' information\n";
+        std::cout << "\n0. Exit\n";
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
@@ -20,7 +21,11 @@ void Menu::displayMenu(Graph ap) {
             case 2:
                 printAvailableFlights(ap);
                 break;
+
             case 3:
+                airportInfoMenu(ap);
+                break;
+            case 0:
                 std::cout << "Thank you for choosing us. See you soon...\n";
                 break;
             default:
@@ -38,3 +43,44 @@ void Menu::printAvailableFlights(Graph ap) {
     int numFlights = ap.getNumEdges();
     std::cout << "The number of available flights: " << numFlights << "\n";
 }
+
+void Menu::countFlightsOutOfAirport(Graph ap) {
+    std::string airportCode;
+    std::cout << "Enter the airport code: ";
+    std::cin >> airportCode;
+
+    Vertex* airportVertex = ap.findVertex(airportCode);
+    if (airportVertex) {
+        int numFlights = airportVertex->getAdj().size();
+        std::cout << "Number of flights out of " << airportCode << ": " << numFlights << std::endl;
+    } else {
+        std::cout << "Airport with code " << airportCode << " not found." << std::endl;
+    }
+}
+
+void Menu::airportInfoMenu(Graph ap){
+    int choice;
+    do {
+
+        std::cout << "\n  I N F O  \n";
+        std::cout << "-----------\n" ;
+        std::cout << "1. Check the number of flights out of an Airport\n";
+
+        std::cout << "\n0. Return to Main Menu\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                countFlightsOutOfAirport(ap);
+                break;
+
+            case 0:
+                displayMenu(ap);
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 3);
+}
+
+
--- src/header/Menu.h
+++ src/header/Menu.h
@@ -10,10 +10,13 @@ class Menu {
 public:
 
     static void displayMenu(Graph ap);
+    static void airportInfoMenu(Graph ap);
 
 private:
     static void printAvailableAirports(Graph ap);
     static void printAvailableFlights(Graph ap);
+    static void countFlightsOutOfAirport(Graph ap);
+    static void countDifferentAirlines(Graph ap);
 
 
 };
