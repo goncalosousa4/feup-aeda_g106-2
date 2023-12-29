6fa13a0cd1dd01823cd5881deb1289e23c09e2ed
Gonçalo Sousa <up202207320@up.pt>
2023-12-29 21:49:11 +0000

    main menu changes

--- main.cpp
+++ main.cpp
@@ -8,14 +8,17 @@
 int main() {
     Graph Airport;
 
-    std::cout << "\nPLEASE WAIT WILL WE TAKE OFF\n\n\n";
+    std::cout << "\nPLEASE WAIT WHILE WE TAKE OFF\n\n";
 
     Loader::loadAirports(Airport, "../dataset/airports.csv");
 
+    _sleep(2500);
+    std::cout << "IT SHOULDN'T TAKE MUCH LONGER\n\n\n";
+
     Loader::loadFlights(Airport, "../dataset/flights.csv");
 
-    std::cout << "FASTEN YOUR SEATBELTS\n\n";
-    _sleep(2000);
+    std::cout << "FASTEN YOUR SEATBELTS, HERE WE GO\n\n";
+    _sleep(1500);
 
     std::cout << " ___________________________________________________\n";
     std::cout << "| --> Welcome to Flight Management Services AED <-- |\n";
